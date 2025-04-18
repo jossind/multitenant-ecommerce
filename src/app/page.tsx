4162335ad3a23ed739fe562a1ab658b5db339831
Jose Juan Tellez <mariachonjunco@gmail.com>
2025-04-18 12:06:27 -0600

    Customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,30 @@
+import { Button } from '@/components/ui/button'
+import { Checkbox } from '@/components/ui/checkbox'
+import { Input } from '@/components/ui/input'
+import { Progress } from '@/components/ui/progress'
+import { Textarea } from '@/components/ui/textarea'
+
 export default function Home() {
-  return <h2 className="text-3xl font-bold underline">Hello World</h2>
+  return (
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant={'elevated'}>I am a button</Button>
+        </div>
+        <div>
+          <Input placeholder="I am a input" />
+        </div>
+        <div>
+          <Progress value={70} />
+        </div>
+        <div>
+          <Textarea placeholder="I am text area" />
+        </div>
+
+        <div>
+          <Checkbox />
+        </div>
+      </div>
+    </div>
+  )
 }
